5a72a58bbf44606b497f684546a89ddd1daa82a6
Ramya.Mariyala <ramya.mariyala@gmail.com>
2021-03-12 22:14:17 -0500

    updated the functions

--- tracker.js
+++ tracker.js
@@ -1,6 +1,6 @@
-const { createPromptModule } = require("inquirer");
 const inquirer = require("inquirer");
 const mysql = requirer("mysql");
+const consoleTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -11,28 +11,220 @@ const connection = mysql.createConnection({
 });
 
 connection.connect((err) => {
-    if(err) throw err;
-    questions ();
+    if (err) throw err;
+    questions();
 });
 
-function question(){
+function question() {
+
+    connection.query("SELECT name, id FROM department", (err,results)=>{
+        if(err) return console.error(err);
+        departments = results;
+    });
+
+    connection.query("SELECT title, id FROM role", (err, results) => {
+        if(err) return console.error(err);
+        roles = results;
+    });
+
     inquirer.prompt([
         {
-            type:"list",
-            message:"What do you want to do?",
-            name:"choice",
-            choices: ["Edit","Add","view","Exit","Remove"]
+            type: "list",
+            message: "What do you want to do?",
+            name: "choice",
+            choices: ["Update Employee","Add User","Add Role","Add Departments","View Employees","View Roles","View Departments","Exit"],
         }
+    ]).then((answers) => {
+        const { choice } = answers;
+        if (choice === "Update Employee") {
+            getEmployeeList();
+        } else if (choice === "Add User") {
+            newUser();
+        } else if (choice === "Add Role") {
+            newRole();
+        } else if (choice === "ADD Departments") {
+            newDepartments();
+        } else if (choice === "View Employees") {
+            viewUsers();
+        } else if (choice === "View Roles") {
+            viewRoles();
+        } else if(choice === "View Departments"){
+            viewDepartments();
+        } else {
+            connection.end();
+        }
+    })
+}
+
+function getEmployeeList(){
+    connection.query("SELECT * FROM employee", (err,results) => {
+        if(err) return console.error(err);
+        getUpdateInfo(results);
+    });
+}
+
+function getUpdateInfo(records){
+    records = records.map(dbData => {
+        return {name: dbData.first_name + " " + dbData.last_name, value: {...dbData}}
+    });
+    roleChoices = roles.map(dbData => {
+        return {name: dbData.title, value: dbData.id};
+    });
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What Employee would you like to update?",
+            name: "role",
+            choices: records
+        },
+        {
+            type: "list",
+            message: "What is the new Role?",
+            name: "role",
+            choices: roleChoices
+        }
+    ]) .then(answers => {
+        let {chosen, role} = answers;
+        role = parseInt(role);
+        updateEmployee(chosen,role);
+    })    
+}
+
+function updateEmployee(chosen, role){
+    const setValue = {role_id: role};
+    const whereValue = { id: chosen.id }
+    connection.query("UPDATE employee SET ? WHERE ?",[setValue, whereValue], err => {
+        if(err) return console.error(err);
+        console.log("Employee Updated");
+
+        questions();
+    });
+}
+
+function newUser(){
+    roleChoices = roles.map(dbData => {
+        return { name:dbData.title, value: dbDataid};
+
+    });
+    inquirer.prompt([
+        {
+            type:"input",
+            name: "first_name",
+            message: "What is the user's first name?"
+        },
+        {
+            type:"input",
+            name: "last_name",
+            message: "What is the user's last name?"
+        },
+        {
+            type: "list",
+            message: "What is their Role?",
+            name: "role_id",
+            choices: roleChoices
+        },
+        {
+            type: "input",
+            name: "manager_id",
+            message: "What is the Manager ID?"
+        },
     ]).then(answers => {
-        switch(answers.choice){
-            case "Edit":
-                return updatePrompt(false);
-            case "Add":
-                return createPrompt(false);
-            case "view":
-                return viewPrompt();
-            case "Exit":
-                return exitApp();    
+        answers.role_id = parseInt(answers.role_id);
+        answers.manager_id = parseInt(answers.manager_id);
+
+        connection.query("INSERT employee SET ?", answers, err => {
+            if(err) return console.error(err);
+            console.log("Employee created.");
+
+            question();
+        });
+
+    });
+}
+
+function viewUsers(){
+    connection.query("SELECT e.first_name, e.last_name, r.title, d.name FROM employee as e left join role as r on e.role_id = r.id left join department as d on r.department_id = d.id", (err, results) => {
+        if(err) return console.error(err);
+        
+        console.table(results)
+        questions();
+    });
+}
+
+function viewRoles(){
+    connection.query("SELECT r.title, r.salary, d.name as department FROM role as r left join department as d on r.department_id = d.id", (err, results) => {
+        if(err) return console.error(err);
+        
+        console.table(results)
+        questions();
+    });
+}
+
+function viewDepartments(){
+    connection.query("SELECT name from department", (err, results) => {
+        if(err) return console.error(err);
+        
+        console.table(results)
+        questions();
+    });
+}
+
+function newRole(){
+    
+    departmentChoices = departments.map(dbData => {
+        return { name: dbData.name, value: dbData.id};
+    });
+    
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "title",
+            message: "What is the role title?"
+        },
+        {
+            type: "input",
+            name: "salary",
+            message: "What is the salary?"
+        },
+        {
+            type: "list",
+            message: "What is Department is this role under?",
+            name: "department_id",
+            choices: departmentChoices
         }
+
+
+    ])
+    .then(answers => {
+        answers.department_id = parseInt(answers.department_id);
+        answers.salary = parseInt(answers.salary);
+
+        connection.query("INSERT role SET ?", answers, err => {
+            if(err) return console.error(err);
+            console.log("Role created.");
+
+            questions();
+        });
+    });
+}
+
+function newDepartment(){
+    
+    
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the Department name?"
+        }
+
+    ])
+    .then(answers => {
+        connection.query("INSERT department SET ?", answers, err => {
+            if(err) return console.error(err);
+            console.log("Department created.");
+
+            questions();
+        });
     });
 }
